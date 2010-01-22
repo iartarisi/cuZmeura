66b6c93849fc03ccd4ea1f989d0e0659d9d0d0b0
Ionuț Arțăriși (mapleoin) <mapleoin@lavabit.com>
2010-01-22 11:06:25 +0200

    moved tests into their own subdir

---
+++ ads/tests/__init__.py
@@ -0,0 +1,2 @@
+from ads.tests.test_serve import ServeTests
+from ads.tests.test_user import UserTests
--- ads/tests/test_serve.py
+++ ads/tests/test_serve.py
@@ -21,7 +21,7 @@ from pristav.ads.models import Impression, Ad, Product
 class ServeTests(TestCase):
     fixtures = ['mydata.json']
 
-    def _test_serve_default_publisher(self, puburl):
+    def _test_get_publisher(self, puburl):
         """
         Test for default publisher.
         """
@@ -32,28 +32,28 @@ class ServeTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assert_(response.context['ad_name'] in ads)
 
-    def test_serve_default_publisher(self):
-        self._test_serve_default_publisher('')
+    def test_no_publisher(self):
+        self._test_get_publisher('')
         
-    def test_serve_pristav_publisher(self):
-        self._test_serve_default_publisher('default')
+    def test_pristav_publisher(self):
+        self._test_get_publisher('default')
 
-    def test_serve_notexists_publisher(self):
+    def test_notexists_publisher(self):
         response = self.client.get('/serve/notfound')
         self.assertEqual(response.status_code, 404)
 
-    def test_serve_size_match_regex(self):
+    def test_size_match_regex(self):
         response = self.client.get('/serve/default/')
         self.assertEqual(response.context['ad_size'], '125x125')
         
         response = self.client.get('/serve/default/125x125')
         self.assertEqual(response.context['ad_size'], '125x125')
 
-    def test_serve_size_not_found(self):
+    def test_size_not_found(self):
         response = self.client.get('/serve/default/0x0')
         self.assertEqual(response.status_code, 404)
 
-    def test_serve_size_mismatch_regex(self):
+    def test_size_mismatch_regex(self):
         invalid_url = ['/serve/x125',
                         '/serve/x125',
                         '/serve/x12y',
@@ -66,7 +66,7 @@ class ServeTests(TestCase):
             response = self.client.get(res)
             self.assertEqual(response.status_code, 404)
 
-    def test_serve_correct_referer_netloc(self):
+    def test_correct_referer_netloc(self):
         response = self.client.get('/serve/default/125x125',
                                    HTTP_REFERER='http://pristav.ceata.org/xmpl')
         impre = Impression.objects.all()[0]
